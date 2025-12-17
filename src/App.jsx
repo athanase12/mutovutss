a01a235fe38719394f2f4436ed45139bd8ed00c2
athanase12 <131745154+athanase12@users.noreply.github.com>
2025-12-17 15:16:23 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -814,7 +814,7 @@ function App() {
                                     className={logoutClass}
                                 >
                                     <div className="relative">
-                                        <CgProfile className="w-6 h-6" />
+                                        <img src="passport.png" className="w-10 h-10 rounded-full"/>
                                         {notifications > 0 && (
                                             <div className="absolute -top-1 -right-1 w-3 h-3 bg-red-500 rounded-full animate-pulse"></div>
                                         )}
