bc07dc1139a31c7e7cf5a892d6d91fdf51b0532c
athanase12 <131745154+athanase12@users.noreply.github.com>
2025-12-19 09:45:43 +0200

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -204,7 +204,7 @@ const LoginPage = ({ setIsAuthenticated, setUserData }) => {
           {/* School Logo/Header */}
           <div className="text-center mb-8">
             <div className="inline-flex items-center justify-center w-20 h-20 bg-gradient-to-r from-blue-500 to-cyan-500 rounded-full mb-4">
-              <FaLock className="text-3xl text-white" />
+              <img src="passport.png" className="text-3xl text-white rounded-full h-20 w-20" />
             </div>
             <h1 className="text-3xl font-bold text-white mb-2">Mutovu TSS Portal</h1>
             <p className="text-gray-300">Secure Login Access Required</p>
